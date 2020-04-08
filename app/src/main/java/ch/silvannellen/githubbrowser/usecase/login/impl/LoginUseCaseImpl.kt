e34b27d820c36e0266bc8b197b0e81d2abe9c33f
Silvan Nellen <silvan.nellen@gmail.com>
2020-04-08 12:11:32 +1000

    [LOGIN] Implementation of login use case.

--- app/src/main/java/ch/silvannellen/githubbrowser/usecase/login/impl/LoginUseCaseImpl.kt
+++ app/src/main/java/ch/silvannellen/githubbrowser/usecase/login/impl/LoginUseCaseImpl.kt
@@ -4,6 +4,8 @@ import ch.silvannellen.githubbrowser.model.common.AuthorizationTokenProvider
 import ch.silvannellen.githubbrowser.model.github.GithubRepository
 import ch.silvannellen.githubbrowser.model.github.api.di.GithubTokenProvider
 import ch.silvannellen.githubbrowser.usecase.login.LoginUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LoginUseCaseImpl @Inject constructor(
@@ -11,6 +13,13 @@ class LoginUseCaseImpl @Inject constructor(
     @GithubTokenProvider private val authorizationTokenProvider: AuthorizationTokenProvider
 ) : LoginUseCase {
 
-    override suspend fun execute(input: LoginUseCase.Credentials): Boolean = true
-
+    override suspend fun execute(input: LoginUseCase.Credentials): Boolean =
+        withContext(Dispatchers.IO) {
+            val result = githubRepository.getUser(input.id, input.accessToken)
+            val loginSuccessful = result.isSuccess()
+            if (loginSuccessful) {
+                authorizationTokenProvider.token = input.accessToken
+            }
+            return@withContext loginSuccessful
+        }
 }
