9979d2a1f0ec5a1fc64a6bae2a971f9d031029c3
Silvan Nellen <silvan.nellen@gmail.com>
2020-04-08 13:58:23 +1000

    [USER] Show list of repositories in user fragment.

--- app/src/main/java/ch/silvannellen/githubbrowser/view/user/UserFragment.kt
+++ app/src/main/java/ch/silvannellen/githubbrowser/view/user/UserFragment.kt
@@ -38,6 +38,12 @@ class UserFragment : GithubBrowserFragment<UserComponent, FragmentUserBinding>()
 
     override fun initialiseView(view: View, savedInstanceState: Bundle?) {
         binding.fragment = this
+        val linearLayoutManager = LinearLayoutManager(context).apply { orientation = LinearLayoutManager.VERTICAL }
+        binding.repositoryList.apply {
+            layoutManager = linearLayoutManager
+            adapter = CodeRepositoriesAdapter()
+            addItemDecoration(DividerItemDecoration(context, linearLayoutManager.orientation))
+        }
     }
 
     override fun observeViewModel(viewModel: BaseViewModel) {
@@ -49,6 +55,9 @@ class UserFragment : GithubBrowserFragment<UserComponent, FragmentUserBinding>()
                 Picasso.get().load(it).into(binding.userAvatar)
             })
 
+            viewModel.repositories.observe(viewLifecycleOwner, Observer {
+                (binding.repositoryList.adapter as CodeRepositoriesAdapter).setRepositories(it)
+            })
         }
     }
 }
